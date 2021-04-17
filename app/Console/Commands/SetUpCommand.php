6f23248f2c97223f904d4741ed3bc14c43f38710
Min Lwin Kyaw <minlwinkyaw307@gmail.com>
2021-04-17 17:51:29 +0300

    init

---
+++ app/Console/Commands/SetUpCommand.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class SetUpCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'setup:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Install the whole project';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(): int
+    {
+        Artisan::call("key:generate");
+        $this->info(Artisan::output());
+
+        Artisan::call("storage:link");
+        $this->info(Artisan::output());
+
+        Artisan::call("db:wipe");
+        $this->info(Artisan::output());
+
+        Artisan::call("migrate:install");
+        $this->info(Artisan::output());
+
+        Artisan::call("migrate");
+        $this->info(Artisan::output());
+
+        Artisan::call("db:seed");
+        $this->info(Artisan::output());
+
+        return 1;
+    }
+}
---
+++ makefile
@@ -0,0 +1,12 @@
+install:
+	php artisan key:generate
+	php artisan storage:link
+	php artisan db:wipe
+	php artisan migrate:install
+	php artisan migrate
+	php artisan db:seed
+
+generate:
+	php artisan ide-helper:generate
+	php artisan ide-helper:models
+	php artisan ide-helper:meta
